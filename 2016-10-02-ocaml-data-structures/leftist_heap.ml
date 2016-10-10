8dbf18b5942c2b11fd4c7e322621ec0af37f0764
kunigami <kunigami@gmail.com>
2016-10-09 18:48:33 -0700

    clean up leftist heap file

--- 2016-10-02-ocaml-data-structures/leftist_heap.ml
+++ 2016-10-02-ocaml-data-structures/leftist_heap.ml
@@ -11,63 +11,54 @@
   The rank of a tree with n nodes is O(log n).
 
 *)
-(* module LeftistHeap = struct *)
-  type elemType = int
-  (* We store the rank as part of the node *)
-  type heapType = Empty | Tree of int * elemType * heapType * heapType
 
-  let rank heap =
-    match heap with
-      | Empty -> 0
-      | Tree(r, _, _, _) -> r
+type elemType = int
+(* We store the rank as part of the node *)
+type heapType = Empty | Tree of int * elemType * heapType * heapType
 
-  let newHeap (elem: elemType): heapType = Tree(1, elem, Empty, Empty)
+let rank heap =
+  match heap with
+    | Empty -> 0
+    | Tree(r, _, _, _) -> r
 
-  (*
-    Construct a heap tree by preserving the leftist property by swapping the
-    left and right children if necessary.
-  *)
-  let makeTree (elem: elemType) (left: heapType) (right: heapType): heapType =
-    if rank left < rank right then Tree((rank left) + 1, elem, right, left)
-    else Tree((rank right) + 1, elem, left, right)
+let newHeap (elem: elemType): heapType = Tree(1, elem, Empty, Empty)
 
-  let rec merge (heapA: heapType) (heapB: heapType): heapType =
-    match (heapA, heapB) with
-      | (heapA, Empty) -> heapA
-      | (Empty, heapB) -> heapB
-      | (Tree(r1, elem1, left1, right1), Tree(r2, elem2, left2, right2)) ->
-        if elem1 < elem2 then makeTree elem1 left1 (merge right1 heapB)
-        else makeTree elem2 left2 (merge right2  heapA)
+(*
+  Construct a heap tree by preserving the leftist property by swapping the
+  left and right children if necessary.
+*)
+let makeTree (elem: elemType) (left: heapType) (right: heapType): heapType =
+  if rank left < rank right then Tree((rank left) + 1, elem, right, left)
+  else Tree((rank right) + 1, elem, left, right)
 
-  let insert (heap: heapType) (elem: elemType): heapType =
-    merge heap (newHeap elem)
+let rec merge (heapA: heapType) (heapB: heapType): heapType =
+  match (heapA, heapB) with
+    | (heapA, Empty) -> heapA
+    | (Empty, heapB) -> heapB
+    | (Tree(r1, elem1, left1, right1), Tree(r2, elem2, left2, right2)) ->
+      if elem1 < elem2 then makeTree elem1 left1 (merge right1 heapB)
+      else makeTree elem2 left2 (merge right2  heapA)
 
-  let findMin (heap: heapType) =
-    match heap with
-      | Empty -> None
-      | Tree(_, elem, _, _) -> Some elem
+let insert (heap: heapType) (elem: elemType): heapType =
+  merge heap (newHeap elem)
 
-  let deleteMin (heap: heapType) =
-    match heap with
-      | Empty -> Empty
-      | Tree (_, _, left, right) -> merge left right
+let findMin (heap: heapType) =
+  match heap with
+    | Empty -> None
+    | Tree(_, elem, _, _) -> Some elem
 
-  let rec equals (heapA: heapType) (heapB: heapType) =
-    match (heapA, heapB) with
-      | (Empty, Empty) -> true
-      | (heapA, Empty) -> false
-      | (Empty, heapB) -> false
-      | (Tree(r1, elem1, left1, right1), Tree(r2, elem2, left2, right2)) ->
-        elem1 == elem2 &&
-        r1 == r2 &&
-        (equals left1 left2) &&
-        (equals right1 right2)
+let deleteMin (heap: heapType) =
+  match heap with
+    | Empty -> Empty
+    | Tree (_, _, left, right) -> merge left right
 
-(* end
-
-(* Examples *)
-
-let h = LeftistHeap.newHeap 10;;
-(* 11 becomes root *)
-let h = LeftistHeap.insert h 11;;
-let h = LeftistHeap.insert h 9;; *)
+let rec equals (heapA: heapType) (heapB: heapType) =
+  match (heapA, heapB) with
+    | (Empty, Empty) -> true
+    | (heapA, Empty) -> false
+    | (Empty, heapB) -> false
+    | (Tree(r1, elem1, left1, right1), Tree(r2, elem2, left2, right2)) ->
+      elem1 == elem2 &&
+      r1 == r2 &&
+      (equals left1 left2) &&
+      (equals right1 right2)
