c72e4a98b7023de21277564e79efda4c5612fc40
kunigami <kunigami@gmail.com>
2017-05-21 11:20:55 -0700

    alternative implementation using tries

--- largest-subsequence-sets/subsequence.ml
+++ largest-subsequence-sets/subsequence.ml
@@ -1,6 +1,8 @@
-open Trie
-open Printf
-open Batteries
+open Trie;;
+open Printf;;
+open BatString;;
+open BatList;;
+open Gc;;
 
 (* words from the dictionary *)
 let file = "/usr/share/dict/words"
@@ -19,57 +21,43 @@ let readFile filename =
     List.rev !lines ;;
 
 let constructTrie (words: string list): trie =
-  List.fold_right Trie.insertString words Trie.empty;;
+  List.fold_left (fun t w -> Trie.insertString w t) Trie.empty words;;
 
 (* Search for all subsequences of s in a trie *)
-let rec searchSubsequenceImpl (s: char list) (trie: trie): char list list = match s with
-  | [] -> []
+let rec searchSubsequenceImpl (s: char list) (trie: trie): trie = match s with
+  | [] -> Trie.empty
   | first_char :: rest_s ->
     let Node(children, _) = trie in
     (* Branch 1: Pick character *)
-    let withChar =  if Trie.ChildList.mem first_char children
+    let trieWithChar =  if Trie.ChildList.mem first_char children
       then
         let nextNode = Trie.ChildList.find first_char children in
-        let matches = searchSubsequenceImpl rest_s nextNode in
+        let matchesTrie = searchSubsequenceImpl rest_s nextNode in
         let Node(_, next_is_word) = nextNode in
-        let fullMatches = if next_is_word then ([] :: matches) else matches in
-        (* Add the current matching character to all matches *)
-        List.map (fun word -> first_char :: word) fullMatches
-      else []
+        let Node(matches, _) = matchesTrie in
+        let nodeWithChar = Node(
+          matches,
+          next_is_word
+        ) in
+        (* Insert the character in front of all words returned *)
+        Node (
+          ChildList.(empty |> add first_char nodeWithChar),
+          false
+        )
+      else Trie.empty
     in
     (* Branch 2: Do not pick character *)
-    let withoutChar = searchSubsequenceImpl rest_s trie
+    let trieWithoutChar = searchSubsequenceImpl rest_s trie
     in
     (* Merge results *)
-    withChar @ withoutChar
+    Trie.merge trieWithChar trieWithoutChar
 
-let rec removeDuplicatesImpl
-  (prevWord: string)
-  (sortedWords: string list)
-: string list = match sortedWords with
-  | [] -> []
-  | nextWord :: sortedWordsRest ->
-    let uniqueWords = removeDuplicatesImpl nextWord sortedWordsRest in
-    if String.equal prevWord nextWord then uniqueWords
-    else nextWord :: uniqueWords
+let searchSubsequence (s: string) (trie: trie): trie =
+  let chars = BatString.to_list s in
+  searchSubsequenceImpl chars trie
 
-let removeDuplicates (words: string list): string list =
-  match (List.sort String.compare words) with
-    | [] -> []
-    | firstWord :: sortedWordsRest ->
-      firstWord :: (removeDuplicatesImpl firstWord sortedWordsRest)
-
-let searchSubsequence (s: string) (trie: trie): string list =
-  let chars = String.to_list s in
-  let results = searchSubsequenceImpl chars trie in
-  List.map String.of_list results |> removeDuplicates
-
-let rec top (n: int) (xs: 'a list): 'a list = match xs with
-  | [] -> []
-  | x :: rest -> if n == 0 then [] else x :: (top (n - 1) rest)
-
-let resultToString (r: string * float): string =
-  "(" ^ (fst r) ^ ", " ^ String.of_float(snd r) ^ ")"
+let resultToString (r: string * int): string =
+  "(" ^ (fst r) ^ ", " ^ BatString.of_int(snd r) ^ ")"
 
 (*
   Given a dictionary (list of words), find, for each word, all the subsequences
@@ -79,14 +67,15 @@ let () =
   let lines = readFile file in
   let trie = constructTrie lines in
   let results = List.map (fun word ->
-    let results = searchSubsequence word trie in
+    let resultTrie = searchSubsequence word trie in
+    let resultsCount = Trie.entriesCount resultTrie in
     (
       word,
-      (float_of_int (List.length results)) /. (float_of_int (String.length word))
+      resultsCount
     )
   ) lines |>
   (* Select the top scores first *)
-  List.sort (fun pair1 pair2 -> compare (snd pair2) (snd pair1)) |>
-  top 100 in
+  List.sort (fun pair1 pair2 -> (snd pair2) - (snd pair1)) |>
+  BatList.take 10 in
   print_endline (String.concat "\n" (List.map resultToString results)
 )
